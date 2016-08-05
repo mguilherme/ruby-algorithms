c1185111bfc138b3af95668074aea636442432a0
Miguel Guilherme <mguilherme82@gmail.com>
2016-08-05 23:53:37 +0100

    Fix typos and block

--- fizzbuzz/lib/fizzbuzz.rb
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -1,17 +1,17 @@
 require 'fizzbuzz/version'
 
 module Fizzbuzz
-  def self.chek_number(number)
+  def self.check_number(number)
     output = ''
     output = 'fizz' if number % 3 == 0
     output = output + 'buzz' if number % 5 == 0
     output.to_s != '' ? output : number.to_s
   end
 
-  def self.chek_range(i)
+  def self.check_range(n)
     output = ''
-    (1..15).each do |i|
-      output = output + chek_number(i).to_s + ' '
+    (1..n).each do |i|
+      output = output + check_number(i).to_s + ' '
     end
     output.strip!
   end
